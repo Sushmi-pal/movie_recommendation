e50113189859ff0c985cac164b0231df585aa5e4
hiiamsid <siddhartha.shrestha@treeleaf.ai>
2022-02-28 21:24:08 +0545

    Recommender optimized

--- category_data.py
+++ category_data.py
@@ -2,30 +2,16 @@
 # Run "python3 manage.py shell" and then write this line one by one so that the data inside Category table will be populated
 from user.models import Category
 
-categories = ['Fiction',
-              'Speculative fiction',
-              'Science Fiction',
-              'Novel',
-              'Fantasy',
-              'Childrens literature',
-              'Mystery',
-              'Suspense',
-              'Historical novel',
-              'Crime Fiction',
-              'Horror',
-              'Historical fiction',
-              'Romance novel',
+categories = ['Horror',
               'Thriller',
               'Young adult literature',
-              'Dystopia',
-              'Detective fiction',
-              'Non-fiction',
-              'Spy fiction',
-              'Satire',
               'Adventure novel',
-              'Alternate history',
-              'Autobiography',
+              'Satire',
+              'Biography',
+              'Dystopia',
               'Gothic fiction',
-              'Biography']
+              'Autobiography',
+              'Alternate history']
+
 for category in categories:
     Category.objects.create(name=category)
